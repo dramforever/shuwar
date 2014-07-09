1665d7809cf42cabf30f5216a4922cc29b507345
dramforever <catmimiao@me.com>
2014-07-09 11:23:07 +0800

    Added specs for runtime

---
+++ spec/runtime_spec.rb
@@ -0,0 +1,27 @@
+require 'rspec'
+
+describe Shuwar::Runtime do
+  def rt; Shuwar::Runtime.new; end
+
+  def run(obj)
+    rt.evaluate obj
+  end
+
+  it "should handle literals" do
+    expect(run 1).to eq 1
+  end
+
+  it "should handle symbols" do
+    expect(run :load).not_to be_nil
+  end
+
+  it "should handle lambdas" do
+    expect(run [:lambda, [:a, :b], :a, :b]).not_to be_nil
+  end
+
+  it "should handle set" do
+    a = rt
+    a.evaluate [:set, :a, 1]
+    expect(a.evaluate :a).to eq 1
+  end
+end
