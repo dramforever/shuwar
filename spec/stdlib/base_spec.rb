b283d35fd82df5a1b0ab5d58bc8f3689da689919
dramforever <catmimiao@me.com>
2014-07-15 22:11:12 +0800

    And here's some specs for them

--- spec/stdlib/base_spec.rb
+++ spec/stdlib/base_spec.rb
@@ -8,21 +8,39 @@ describe Shuwar::Stdlib::Base do
     rt.evaluate obj
   end
 
-  it "has a working load" do
+  it "should have a working load" do
     run [:load, [:quote, :nokogiri]]
   end
 
-  it "has a working begin" do
+  it "should have a working begin" do
     expect(run [:begin, 1, 2]).to eq 2
   end
 
-  it "has a working print" do
+  it "should have a working print" do
     run [:print, 1]
   end
 
-  it "has a working puts" do
+  it "should have a working puts" do
     run [:puts, 1]
   end
 
-  # Marcos seems to have been tested in runtime_spec
+  it "should have a working load_file" do
+    run [:load_file, "sample.swr"]
+  end
+
+  it "should have a working fetch" do
+    r = rt
+    expect(run [:fetch, r, [:quote, :load]]).not_to be_nil
+  end
+
+  it "should have a working import" do
+    r = rt
+    r.evaluate [:import, :sample]
+    expect(r.evaluate :sample).not_to be_nil
+
+    r.evaluate [:import, :sample, :main_content]
+    expect(r.evaluate :main_content).not_to be_nil
+  end
+
+  # Other marcos seems to have been tested in runtime_spec
 end
